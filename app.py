759d4deae6a6beb6094bc1c6b1f11793de193df4
Kai <57695473+KaiHsuan-Liu@users.noreply.github.com>
2020-07-03 02:26:34 +0800

    Create README.md

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import pymongo
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
 
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+
+db = myclient['sth']
+collect = db['record']
+
+# SOMETHING = [
+#     {
+#         'title': 'Kai',
+#         'comment': 'A handsome boy',
+#         'check': True
+#     },
+#     {
+#         'title': 'Mi',
+#         'comment': 'A cute girl',
+#         'check': False
+#     }
+# ]
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello Flask!</h1>"
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    res = request.get_json()
-    print(res)
-    for i in res:
-        print(type(res[i]), res[i])
+@app.route('/Sth', methods=['GET', 'POST'])
+def sth():
+    SOMETHING = []
+    if request.method == 'POST':
+        post_data = request.get_json()
+        collect.insert_one(post_data)
+    else:
+        cursor = collect.find()
+        for x in cursor:
+            tmp = {}
+            tmp['title'] = x['title']
+            tmp['comment'] = x['comment']
+            tmp['check'] = x['check']
+            SOMETHING.append(tmp)
 
-    return 'PASS'
+    return jsonify({
+        'status': 'success',
+        'something': SOMETHING
+    })
 
-app.run()
+if __name__ == '__main__':
+    app.run()
 
 
