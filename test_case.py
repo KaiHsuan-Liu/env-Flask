49ecec540386c7ec5949cf258bdd6785b8879216
Kai Liu <love83030414@gmail.com>
2020-07-14 01:20:21 +0800

    Add Unittest.
    Signed-off-by: Kai Liu <love83030414@gmail.com>

---
+++ test_case.py
@@ -0,0 +1,18 @@
+import unittest
+import json
+from app import app
+
+class ToDoList(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
+    def test_home(self):
+        res = self.client.get("/")
+        print(res.data)
+        assert b'<h1>Hello Flask!</h1>' in res.data
+    def test_api_sth(self):
+        res = self.client.get("/Sth")
+        print(res.data)
+
+if __name__ == '__main__':
+    unittest.main()
