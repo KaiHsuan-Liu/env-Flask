d3659b20566e7d67a82870db4be4f39e3d8fec44
Kai <57695473+KaiHsuan-Liu@users.noreply.github.com>
2020-07-15 10:53:53 +0800

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -7,11 +7,15 @@ class ToDoList(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.client = app.test_client()
-
+        
+    def tearDown(self):
+        self.client = None
+        
     def test_home(self):
         res = self.client.get("/")
         print(res.data)
-        assert b'<h1>Hello Flask!</h1>' in res.data
+        # assert b'<h1>Hello Flask!</h1>' in res.data
+        self.assertEqual(res.data, b'<h1>Hello Flask!</h1>')
 
     def test_api_sth(self):
         res = self.client.get("/Sth")
